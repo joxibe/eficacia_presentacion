cd108de80ab592fea12618b2e2eb824ab3f55027
Jose alfredo Rosero <jose_rosero@eficacia.com.co>
2025-02-05 14:54:46 -0500

    Mejoras en diseño e informacion

--- scripts.js
+++ scripts.js
@@ -22,4 +22,13 @@ document.addEventListener("DOMContentLoaded", () => {
             card.classList.add("active");
         });
     });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuToggle = document.querySelector('.menu-toggle');
+        const navMenu = document.querySelector('.nav-menu');
+    
+        menuToggle.addEventListener('click', function () {
+            navMenu.classList.toggle('active');
+        });
+    });    
 });
